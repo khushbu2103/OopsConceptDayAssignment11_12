266f7157db7657c5da7016a61222005b30994997
khushbu <khushbusoni475@gmail.com>
2023-06-19 16:55:20 +0530

    [khushbu] modify find number of duplicate element

---
+++ OopsConceptDayAssignment11_12/DuplicateElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace OopsConceptDayAssignment11_12
+{
+    internal class DuplicateElement
+    {
+        public static int RepeatedDuplicateElement(int[] arr)
+        {
+
+            //int[] arr = new int[] { 1, 2, 3, 4, 5, 2, 3, 4, 2, 3, 3, 0, 0 };
+            int[] duplicateElements = new int[arr.Length];
+            int duplicateCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (isDuplicate)
+                {
+                    bool alreadyCounted = false;
+                    for (int k = 0; k < duplicateCount; k++)
+                    {
+                        if (arr[i] == duplicateElements[k])
+                        {
+                            alreadyCounted = true;
+                            break;
+                        }
+                    }
+                    if (!alreadyCounted)
+                    {
+                        duplicateElements[duplicateCount] = arr[i];
+                        duplicateCount++;
+                    }
+                }
+            }
+            
+            return duplicateCount;
+        }
+    }
+    
+}
--- OopsConceptDayAssignment11_12/Program.cs
+++ OopsConceptDayAssignment11_12/Program.cs
@@ -10,6 +10,25 @@ namespace OopsConceptDayAssignment11_12
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("please Choose any Program");
+            Console.WriteLine("1.Duplicate number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the size of array");
+                    int len = Convert.ToInt32(Console.ReadLine()); //length of array
+                    int[] arr = new int[len];
+                    for (int i = 0; i < len; i++)
+                    {
+                        Console.Write("Enter element number {0} of array:", i + 1);
+                        arr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine("Number of elements repeating more than once in the array: " +  DuplicateElement.RepeatedDuplicateElement(arr));
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
